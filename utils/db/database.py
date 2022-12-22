45f166cd7e4e5a32a144dbbaba9b0c7eb5b9c569
DaniilMP3 <daniilpurisov390@gmail.com>
2022-12-22 17:39:59 +0200

    database queries function

--- utils/db/database.py
+++ utils/db/database.py
@@ -4,18 +4,48 @@ import sqlite3
 class Database:
     def __init__(self, path):
         self.connection = sqlite3.connect(path)
+        self.connection.execute("PRAGMA foreign_keys = on")
+        self.connection.commit()
+
         self.cursor = self.connection.cursor()
 
     def create_tables(self):
-        self.cursor.execute("CREATE TABLE IF NOT EXISTS users"
+        """
+        https://imgur.com/a/wenCbOz
+        """
+
+        self.cursor.execute("CREATE TABLE IF NOT EXISTS specialities("
                             "id INT PRIMARY KEY,"
-                            "user_id INT,"
-                            "full_name VARCHAR,"
-                            "sex VARCHAR,"
-                            "speciality VARCHAR,"
-                            "course VARCHAR,"
-                            "group VARCHAR,"
-                            "meetings_count INT")
+                            "speciality VARCHAR)")
+
+        self.cursor.execute("CREATE TABLE IF NOT EXISTS users_groups("
+                            "id INT PRIMARY KEY,"
+                            "user_group VARCHAR,"
+                            "parent_speciality INTEGER,"
+                            "FOREIGN KEY(parent_speciality) REFERENCES specialities(id) ON DELETE CASCADE)")
+
+        self.cursor.execute("CREATE TABLE IF NOT EXISTS users("
+                            "id INTEGER PRIMARY KEY,"
+                            "user_id INTEGER NOT NULL,"
+                            "full_name VARCHAR NOT NULL,"
+                            "sex VARCHAR NOT NULL,"
+                            "course INTEGER,"
+                            "user_group INTEGER,"
+                            "meetings_count INTEGER, "
+                            "FOREIGN KEY(user_group) REFERENCES users_groups(id) ON DELETE CASCADE)")
+
+        self.cursor.execute("CREATE TABLE IF NOT EXISTS links("
+                            "id INTEGER PRIMARY KEY,"
+                            "link VARCHAR NOT NULL)")
+
+        self.cursor.execute("CREATE TABLE IF NOT EXISTS meetings("
+                            "id INTEGER PRIMARY KEY,"
+                            "first_user INTEGER,"
+                            "second_user INTEGER,"
+                            "link INTEGER,"
+                            "FOREIGN KEY(link) REFERENCES links(id),"
+                            "FOREIGN KEY(first_user) REFERENCES users(id),"
+                            "FOREIGN KEY(second_user) REFERENCES users(id) ON DELETE CASCADE)")
 
     async def fetchone(self, query, args=None):
         if args:
@@ -38,3 +68,10 @@ class Database:
             res = self.cursor.execute(query)
         return res.fetchmany(size)
 
+    async def query(self, query, args=None):
+        if args:
+            self.cursor.execute(query, args)
+        else:
+            self.cursor.execute(query)
+
+        self.connection.commit()
