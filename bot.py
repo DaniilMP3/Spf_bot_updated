bb07dddd0abd9b5a4f089e4e734e5aad2560fbc3
DaniilMP3 <daniilpurisov390@gmail.com>
2022-12-22 17:36:18 +0200

    set up middleware(antiflood)

--- bot.py
+++ bot.py
@@ -1,9 +1,18 @@
-from aiogram import executor
-from create_bot import dp, db
+from aiogram import executor, types
+from create_bot import dp
+from handlers import *
+from middleware import ThrottlingMiddleware
+
+
+@dp.message_handler(commands=['start'], state=None)
+async def start(message: types.Message):
+    await message.answer("Start. If you are not registered yet, enter /registration.")
 
 
 async def on_startup(_):
     db.create_tables()
+    dp.setup_middleware(ThrottlingMiddleware())
     print("Online")
 
-executor.start_polling(dp, skip_updates=True, on_startup=True)
+
+executor.start_polling(dp, skip_updates=True, on_startup=on_startup)
---
+++ middleware/__init__.py
@@ -0,0 +1,1 @@
+from .antiflood import ThrottlingMiddleware
