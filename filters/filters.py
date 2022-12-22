f7b4939082b855b2165a70df78674147047cc750
DaniilMP3 <daniilpurisov390@gmail.com>
2022-12-22 17:37:09 +0200

    a few new filters

--- filters/filters.py
+++ filters/filters.py
@@ -1,9 +1,29 @@
 from aiogram.dispatcher.filters import Filter
 from aiogram.types import Message
-from create_bot import db
+from create_bot import db, ADMINS
 
 
 class IsRegistered(Filter):
     async def check(self, message: Message):
         user_id = message.from_user.id
-        return bool(db.fetchone("SELECT * FROM users WHERE user_id = ?", user_id))
+
+        return bool(await db.fetchone("SELECT * FROM users WHERE user_id = ?", (user_id,)))
+
+
+class IsNotRegistered(Filter):
+    async def check(self, message: Message):
+        user_id = message.from_user.id
+
+        return not bool(await db.fetchone("SELECT * FROM users WHERE user_id = ?", (user_id,)))
+
+
+class IsAdmin(Filter):
+    async def check(self, message: Message):
+        user_id = message.from_user.id
+        return user_id in ADMINS
+
+
+class IsUser(Filter):
+    async def check(self, message: Message):
+        user_id = message.from_user.id
+        return user_id not in ADMINS
