3a0e8ccf664c9809818fca2d25a24dbde6bb572f
DaniilMP3 <daniilpurisov390@gmail.com>
2022-12-22 17:38:13 +0200

    new state for editing hierarchy tree

--- states/states.py
+++ states/states.py
@@ -4,6 +4,11 @@ from aiogram.dispatcher.filters.state import State, StatesGroup
 class Register(StatesGroup):
     full_name = State()
     sex = State()
-    speciality = State()
     course = State()
+    speciality = State()
+    group = State()
+
+
+class EditHierarchy(StatesGroup):
+    speciality = State()
     group = State()
