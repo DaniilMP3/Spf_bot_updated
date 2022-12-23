5f2c3074d97ea025cccce36b4654bc6041b5142f
DaniilMP3 <daniilpurisov390@gmail.com>
2022-12-23 15:39:35 +0200

    start making hierarchy system

--- handlers/admin/edit_hierarchy.py
+++ handlers/admin/edit_hierarchy.py
@@ -1,29 +1,36 @@
+import re
 from filters import IsAdmin
 from aiogram import types
 from create_bot import dp, db
 from middleware.antiflood import rate_limit
 from aiogram.dispatcher import FSMContext
 from aiogram.dispatcher.filters import Text
-from states import EditHierarchy
+from hierarchy import Hierarchy
 
 
-@dp.message_handler(IsAdmin(), commands=['edit_hierarchy'])
-@rate_limit(2, 'edit_hierarchy')
-async def show_specialities(message: types.Message):
-    specialities = await db.fetchall("SELECT * FROM specialities")
-
-    if not specialities:
-        await message.answer("There's no specialities. Enter the new speciality:")
+@dp.message_handler(IsAdmin(), commands=['show_hierarchy'])
+@rate_limit(2, 'show_hierarchy')
+async def show_hierarchy(message: types.Message):
+    res = db.fetchall("SELECT * FROM specialities")
+    if not res:
+        await message.answer("No hierarchy now.")
         return
 
-    for s in specialities:
-        await message.answer(s.speciality, reply_markup=types.InlineKeyboardMarkup().add(
-            types.InlineKeyboardButton("Delete", callback_data=f"delete_{s.id}"),
-            types.InlineKeyboardButton("Edit", callback_data=f"edit_{s.id}"),
-            types.InlineKeyboardButton("Next step", callback_data=f"nextstep_{s.id}")
-        ))
+
+@dp.message_handler(commands=['create_hierarchy'])
+@rate_limit(3, 'create_hierarchy')
+async def create_hierarchy(message: types.Message):
+    msg = message.text
+    split_msg = msg.split()
+    split_len = len(split_msg)
+    if split_len not in range(2, 4):
+        await message.answer("Incorrect format.")
+        return
+
+
 
 
 @dp.inline_handler(Text(startswith=['delete']))
 async def delete_speciality(query: types.InlineQuery):
     print(query)
+
