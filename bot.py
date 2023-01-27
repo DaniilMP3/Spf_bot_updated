c925573cc1f8c98dca489fe1354b3d5bc19ca5e6
Aresmp3 <daniilpurisov390@gmail.com>
2023-01-27 21:26:14 +0200

    run TCP sockets in one event loop with bot

--- bot.py
+++ bot.py
@@ -1,18 +1,81 @@
-from aiogram import executor, types
+from aiogram import types
 from create_bot import dp
 from handlers import *
 from middleware import ThrottlingMiddleware
+from middleware.antiflood import rate_limit
+import asyncio
+from aiohttp import web
+from api.event_sync.event_sync import event_app, event_routes
+from keyboards import cancel_cross
+from aiogram.dispatcher.filters import Text
+from aiogram.dispatcher import FSMContext
 
 
-@dp.message_handler(commands=['start'], state=None)
+@dp.message_handler(commands=['start'], state=None, chat_type=types.ChatType.PRIVATE)
+@rate_limit(3)
 async def start(message: types.Message):
-    await message.answer("Start. If you are not registered yet, enter /registration.")
+    await message.answer("Start. If you are not registered yet, enter /register.")
 
 
-async def on_startup(_):
-    db.create_tables()
+@dp.message_handler(state='*', commands="cancel", chat_type=types.ChatType.PRIVATE)
+@dp.message_handler(Text(equals=cancel_cross), state="*", chat_type=types.ChatType.PRIVATE)
+@rate_limit(3)
+async def cancel(message: types.Message, state: FSMContext):
+    current_state = await state.get_state()
+
+    if current_state is None:
+        return
+    await state.finish()
+    await message.answer("Canceled", reply_markup=types.ReplyKeyboardRemove())
+
+
+loop = asyncio.new_event_loop()
+asyncio.set_event_loop(loop)
+
+runners = []
+
+
+async def start_site(application, host, port, routes=None):
+    if routes:
+        application.add_routes(routes)
+
+    runner = web.AppRunner(application)
+    runners.append(runner)
+
+    await runner.setup()
+    site = web.TCPSite(runner, host, port)
+    print(f"Socket works on {host}:{port}")
+
+    await site.start()
+
+
+async def on_startup():
     dp.setup_middleware(ThrottlingMiddleware())
-    print("Online")
+    db.create_tables()
 
 
-executor.start_polling(dp, skip_updates=True, on_startup=on_startup)
+async def start_polling():
+    print("Bot Online")
+    await dp.start_polling()
+
+
+async def main():
+    await asyncio.gather(start_polling(),
+                         start_site(event_app, "localhost", 8000, event_routes))
+
+
+try:
+    loop.create_task(on_startup())
+    loop.run_until_complete(main())
+except:
+    pass
+finally:
+    dp.stop_polling()
+    for runner in runners:
+        loop.run_until_complete(runner.cleanup())
+
+    print("Shut down. Bye!")
+
+
+
+
