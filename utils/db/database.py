22c43c48ebadb77df622a3341f9ca40c35f3e4b0
Aresmp3 <daniilpurisov390@gmail.com>
2023-01-29 19:41:05 +0200

    remove status fields from meetings table and make Database class atribues protected

--- utils/db/database.py
+++ utils/db/database.py
@@ -3,86 +3,84 @@ import sqlite3
 
 class Database:
     def __init__(self, path):
-        self.connection = sqlite3.connect(path)
-        self.connection.execute("PRAGMA foreign_keys = ON")
-        self.connection.commit()
+        self._connection = sqlite3.connect(path)
+        self._connection.execute("PRAGMA foreign_keys = ON")
+        self._connection.commit()
 
-        self.cursor = self.connection.cursor()
+        self._cursor = self._connection.cursor()
 
     def create_tables(self):
         """
         https://imgur.com/a/wenCbOz
         """
 
-        self.cursor.execute("CREATE TABLE IF NOT EXISTS specialities("
-                            "id INTEGER PRIMARY KEY NOT NULL,"
-                            "speciality VARCHAR UNIQUE COLLATE NOCASE)")
+        self._cursor.execute("CREATE TABLE IF NOT EXISTS specialities("
+                             "id INTEGER PRIMARY KEY NOT NULL,"
+                             "speciality VARCHAR UNIQUE COLLATE NOCASE)")
 
-        self.cursor.execute("CREATE TABLE IF NOT EXISTS users_groups("
-                            "id INTEGER PRIMARY KEY NOT NULL,"
-                            "user_group VARCHAR,"
-                            "parent_speciality INTEGER,"
-                            "FOREIGN KEY(parent_speciality) REFERENCES specialities(id) ON DELETE CASCADE,"
-                            "UNIQUE(user_group COLLATE NOCASE, parent_speciality))")
+        self._cursor.execute("CREATE TABLE IF NOT EXISTS users_groups("
+                             "id INTEGER PRIMARY KEY NOT NULL,"
+                             "user_group VARCHAR,"
+                             "parent_speciality INTEGER,"
+                             "FOREIGN KEY(parent_speciality) REFERENCES specialities(id) ON DELETE CASCADE,"
+                             "UNIQUE(user_group COLLATE NOCASE, parent_speciality))")
 
-        self.cursor.execute("CREATE TABLE IF NOT EXISTS users("
-                            "id INTEGER PRIMARY KEY NOT NULL,"
-                            "telegram_id INTEGER NOT NULL,"
-                            "full_name VARCHAR NOT NULL,"
-                            "sex VARCHAR NOT NULL,"
-                            "course INTEGER,"
-                            "user_group INTEGER,"
-                            "meetings_count INTEGER, "
-                            "FOREIGN KEY(user_group) REFERENCES users_groups(id) ON DELETE CASCADE)")
+        self._cursor.execute("CREATE TABLE IF NOT EXISTS users("
+                             "id INTEGER PRIMARY KEY NOT NULL,"
+                             "telegram_id INTEGER NOT NULL,"
+                             "full_name VARCHAR NOT NULL,"
+                             "sex VARCHAR NOT NULL,"
+                             "course INTEGER,"
+                             "user_group INTEGER,"
+                             "meetings_count INTEGER, "
+                             "FOREIGN KEY(user_group) REFERENCES users_groups(id) ON DELETE CASCADE)")
 
-        self.cursor.execute("CREATE TABLE IF NOT EXISTS meetings("
-                            "id INTEGER PRIMARY KEY NOT NULL,"
-                            "first_user INTEGER,"
-                            "second_user INTEGER,"
-                            "first_link TEXT,"
-                            "second_link TEXT,"
-                            "time VARCHAR,"
-                            "first_user_status VARCHAR(25),"
-                            "second_user_status VARCHAR(25),"
-                            "FOREIGN KEY(first_user) REFERENCES users(id),"
-                            "FOREIGN KEY(second_user) REFERENCES users(id) ON DELETE CASCADE)")
+        self._cursor.execute("CREATE TABLE IF NOT EXISTS meetings("
+                             "id INTEGER PRIMARY KEY NOT NULL,"
+                             "first_user INTEGER,"
+                             "second_user INTEGER,"
+                             "first_link TEXT,"
+                             "second_link TEXT,"
+                             "time VARCHAR,"
+                             "FOREIGN KEY(first_user) REFERENCES users(id),"
+                             "FOREIGN KEY(second_user) REFERENCES users(id) ON DELETE CASCADE)")
 
     def fetchone(self, query, args=None):
         if args:
-            self.cursor.execute(query, args)
+            self._cursor.execute(query, args)
         else:
-            self.cursor.execute(query)
-        return self.cursor.fetchone()
+            self._cursor.execute(query)
+        return self._cursor.fetchone()
 
     def fetchall(self, query, args=None):
         if args:
-            self.cursor.execute(query, args)
+            self._cursor.execute(query, args)
         else:
-            self.cursor.execute(query)
-        return self.cursor.fetchall()
+            self._cursor.execute(query)
+        return self._cursor.fetchall()
 
     def fetchmany(self, query, size, args=None):
         if args:
-            self.cursor.execute(query, args)
+            self._cursor.execute(query, args)
         else:
-            self.cursor.execute(query)
-        return self.cursor.fetchmany(size)
+            self._cursor.execute(query)
+        return self._cursor.fetchmany(size)
 
     def query(self, query, args=None):
         if args:
-            self.cursor.execute(query, args)
+            self._cursor.execute(query, args)
         else:
-            self.cursor.execute(query)
+            self._cursor.execute(query)
 
-        if self.cursor.rowcount == 1:
+        if self._cursor.rowcount == 1:
 
-            self.connection.commit()
+            self._connection.commit()
             return True
         return False
 
     def in_database(self, table, column, value):
-        self.cursor.execute(f"SELECT 1 FROM {table} WHERE {column} = ?", (value,))
-        if self.cursor.fetchone():
+        self._cursor.execute(f"SELECT 1 FROM {table} WHERE {column} = ?", (value,))
+        if self._cursor.fetchone():
             return True
 
         return False
